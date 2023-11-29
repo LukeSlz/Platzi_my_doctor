ea159374b147150032ac43e40e6635dc4389bdac
Luke Salazar <lucassy07@outlook.com>
2023-11-29 16:21:02 -0500

    Patient.java init

--- MyMedicalAppointment/src/Doctor.java
+++ MyMedicalAppointment/src/Doctor.java
@@ -1,15 +1,18 @@
 public class Doctor {
     static int id = 0;      //Auto Increment. al ser Static el valor prevalece
     String name;
+    String email;
     String speciality;
 
     Doctor(){
         System.out.println("Building the object 'Doctor'");
         id++;
     }
-    Doctor(String name){
+    Doctor(String name, String speciality){
         System.out.println("The assigned doctor is:" + name);
         id++;
+        this.name = name;
+        this.speciality = speciality;
     }
     //behaviour
     public void showName(){
--- MyMedicalAppointment/src/Main.java
+++ MyMedicalAppointment/src/Main.java
@@ -4,6 +4,14 @@ import static ui.UiMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        showMenu();
+        //showMenu();
+
+        Doctor myDoctor = new Doctor("Luke", "XR");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
+
+        Patient myPatient = new Patient("Alejandra", "ale@mail.com");
+
+
     }
 }
